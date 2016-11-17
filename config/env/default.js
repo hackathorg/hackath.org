d422a98a363b7967ea14597cfd3ebd4979fd29a4
Michael Holtom <m.p.holtom@warwick.ac.uk>
2016-11-17 01:26:40 +0000

    Update environment variables

--- config/env/default.js
+++ config/env/default.js
@@ -19,7 +19,7 @@ module.exports = {
     }
   },
   hostname: process.env.HOST || process.env.HOSTNAME,
-  db: process.env.MONGOHQ_URL,
+  db: process.env.MONGODB_URI,
   templateEngine: 'swig',
 
   // The secret should be set to a non-guessable string that
