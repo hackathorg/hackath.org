3981b9faafea424fd1934c17d02e4432f89f6b63
Michael Holtom <m.p.holtom@warwick.ac.uk>
2016-11-24 04:28:07 +0000

    Heroku support

--- gulp/production.js
+++ gulp/production.js
@@ -5,12 +5,16 @@ var gulpLoadPlugins = require('gulp-load-plugins');
 var path = require('path');
 var _ = require('lodash');
 var plugins = gulpLoadPlugins();
+var herokuTasks = ['clean', 'webpackBuildProd'];
 var defaultTasks = ['clean', 'cssmin', 'uglify', 'prodServe'];
 var assets = require('../config/assets.json');
-
+var gutil = require('gulp-util');
+var webpackProdConfig = require('../webpack.production.config.js')
+var webpack = require('webpack');
 gulp.task('env:production', function () {
   process.env.NODE_ENV = 'production';
 });
+
 
 function tokenizeConfig (config) {
   var destTokens = _.keys(config)[0].split('/');
@@ -21,6 +25,7 @@ function tokenizeConfig (config) {
     destFile: destTokens[destTokens.length - 1]
   };
 }
+gulp.task('webpackBuildProd',['cssmin','uglify'], webpackBuildProd);
 
 gulp.task('cssmin', function () {
   console.log('in cssmin');
@@ -50,14 +55,30 @@ gulp.task('uglify', function () {
   }
 });
 
-gulp.task('prodServe', ['env:production'], function () {
-  plugins.nodemon({
-    script: 'server.js',
-    ext: 'html js',
-    env: {
-      'NODE_ENV': 'production'
-    },
-    ignore: ['./node_modules/**']
+
+// var ngAnnotatePlugin = require('ng-annotate-webpack-plugin')
+
+
+////////////////////////////////////////////////////////////////////
+
+// modify some webpack config options
+var prodConfig = Object.create(webpackProdConfig);
+prodConfig.devtool = 'sourcemap';
+prodConfig.debug = true;
+// create a single instance of the compiler to allow caching
+var prodCompiler = webpack(prodConfig);
+
+function webpackBuildProd (callback) {
+  // run webpack
+  prodCompiler.run(function (err, stats) {
+    if (err) {
+      throw new gutil.PluginError('webpackBuild', err);
+    }
+    gutil.log('webpackBuild', stats.toString({
+      colors: true
+    }));
+    callback()
   })
-})
+}
 gulp.task('production', defaultTasks)
+gulp.task('heroku:production', herokuTasks)
