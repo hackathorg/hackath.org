9f0c50c25260d5aa7be7fca6ad12aed511702772
mahoneyj2 <jamesmahoney200@hotmail.com>
2017-05-08 18:16:23 +0100

    More minor fixes for Michael

--- packages/custom/hackathorg-profile/public/controllers/hackathorgProfile.js
+++ packages/custom/hackathorg-profile/public/controllers/hackathorgProfile.js
@@ -79,6 +79,10 @@
         /* This user */
         $scope.user = HackathorgProfile.profiles.show({userId:$scope.userToId($scope.vieweduser)}, function(){
             $scope.user.icon = 'https://www.gravatar.com/avatar/' +  MD5($scope.user.email) + '?s=152&r=pg&d=identicon';
+            $scope.mentors = a.users.filter(function(x){return x.role.toLowerCase() === 'mentor';});
+            $scope.attendees = a.users.filter(function(x){return x.role.toLowerCase() === 'attendee'})
+            $scope.sponsors = a.users.filter(function(x){return x.role.toLowerCase() === 'sponsor'})
+            $scope.organisers = a.users.filter(function(x){return x.role.toLowerCase() === 'organiser'})
         });
 
         $scope.submit = function(){
